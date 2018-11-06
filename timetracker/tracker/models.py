2b4b3ad738ce400bbb19608d4b80e7c6ee8b0c81
Brittain Hard <brittainhard@gmail.com>
2018-11-06 13:46:49 -0600

    Activity name must be unique.

--- timetracker/tracker/models.py
+++ timetracker/tracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Activity(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def get_absolute_url(self):
         return "/"
